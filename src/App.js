e6896b0bfe0647f2cfa249cc053818c2efedb9fd
sofiaa-garcia14 <97136109+sofiaa-garcia14@users.noreply.github.com>
2022-11-29 19:10:48 -0300

    NavBar.js y CardWidget.js agregado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <NavBar />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import CartWidget from '../CartWidget/CartWidget';
+
+const NavBar = () => {
+  return (
+    <nav className='navbar navbar-expand-lg bg-light'>
+      <div className='container'>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarTogglerDemo03'
+          aria-controls='navbarTogglerDemo03'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <a className='navbar-brand' href='/'>
+          Music Live
+        </a>
+        <div className='collapse navbar-collapse' id='navbarTogglerDemo03'>
+          <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+            <li className='nav-item'>
+              <a className='nav-link active' aria-current='page' href='/'>
+                Inicio
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='/'>
+                Artistas
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='/'>
+                Categorías
+              </a>
+            </li>
+          </ul>
+          <CartWidget />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
